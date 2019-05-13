3dda7b6090bbed91a7b190bbda9f81bc4210af01
Anneke Lohmann <anneke.lohmann@gmx.de>
2019-05-13 11:41:56 +0200

    calculate average sales per category

--- reducer.py
+++ reducer.py
@@ -20,6 +20,8 @@ import sys
 # Cash  455.51
 
 # Sum of all sales (values) is initialized with zero, we just started
+sum_of_values = 0
+# Count of purchases
 count_of_values = 0
 
 # Previous key is initialized with None, we just started
@@ -47,17 +49,19 @@ for line in sys.stdin:
         # Key and value are seperated by a tab (\t)
         # Line ends with new line (\n)
         if count_of_values>114:
-            sys.stdout.write("{0}\t{1}\n".format(previous_key, count_of_values))
+            sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values/count_of_values))
         # Sum of sales starts again with 0
         count_of_values = 0
+        sum_of_values = 0
 
     # Add the value to the total sales
     # a += b is the same as a = a + b
     # the float function transforms the value
     # to a float data type (like decimal)
     count_of_values += 1
+    sum_of_values += float(value)
     # the previous key for the next iteration is the current key of the this iteration
     previous_key = key
 
 # write the last result to stdout
-sys.stdout.write("{0}\t{1}\n".format(previous_key, count_of_values))
+sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values/count_of_values))
